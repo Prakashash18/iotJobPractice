581af843d527ba3b8257fc4792f7b08c68ceb31c
prakash <prakash@prakashs-MacBook-Pro.local>
2019-06-04 08:39:47 +0800

    t

---
+++ job_5_error_1/job_5_error_1.ino
@@ -0,0 +1,19 @@
+
+int first_number_in_$ = 3;
+double (second_number) = 1.2; 
+float third _number = 3.14;
+
+void setup() {
+  // put your setup code here, to run once: 
+  Serial.begin(9600);
+  
+  Serial.println(first_number_in_$);  
+  Serial.println((second_number));
+  Serial.println(Third_number);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
