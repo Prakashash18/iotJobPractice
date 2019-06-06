8244af6e1f32d816d9ef76753ac3dacea6253dba
prakash <prakash@prakashs-MacBook-Pro.local>
2019-06-06 14:19:43 +0800

    test

--- job_8_error_1/job_8_error_1.ino
+++ job_8_error_1/job_8_error_1.ino
@@ -1,15 +1,19 @@
-
-char value = 0;
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello World");
+
+  a =  / 2;
+  
+  a = a + 2;
+  Serial.println(a);
+  a += ;       
+  Serial.println(a);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available() > 0) 
-      value = Serial.read();
-      Serial.print(value);
+  
+
 }
