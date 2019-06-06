c8dc991c3508aee478cbc5a09baa20eed1f6021e
prakash <prakash@prakashs-MacBook-Pro.local>
2019-06-06 10:29:47 +0800

    test

--- job_7_error_1/job_7_error_1.ino
+++ job_7_error_1/job_7_error_1.ino
@@ -1,15 +1,27 @@
-
-char value = 0;
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello World");
+
+  //term 
+  int a = 6 ++ //6;  // this is addition
+  int b = 6 -= 8; // this is subtraction
+  int c = 6 ** 6; // this is multiplication
+  int d = 9 // 2; // this is division 
+  int e = 9 / 2; // this is to find remainder (from division) 
+
+  float float = 7 / 2;
+
+  Serial.println(a);
+  Serial.println(b);
+  Serial.println(c);
+  Serial.println(d);
+  Serial.println(e);
+  Serial.println(f);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available() > 0) 
-      value = Serial.read();
-      Serial.print(value);
+  
+
 }
